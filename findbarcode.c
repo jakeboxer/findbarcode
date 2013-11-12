1aea8b61acb07b209328126c9faa878b25ab0464
Jake Boxer <jake@github.com>
2013-11-12 01:00:31 -0800

    Clean shit up
    * components -> bytes_per_ixel
    * Use STBI_default instead of 0 for required number of bytes per pixel
    * Handle errors from stbi_load
    * Loop over image data and print results
    * Free memory from stbi_load
    * return 0 from main()

--- findbarcode.c
+++ findbarcode.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include "stb_image.c"
 
+#define FILENAME "wikipedia_barcode.png"
+
 int main(int argc, char **argv)
 {
-	int width, height, components;
-	unsigned char *data = stbi_load("wikipedia_barcode.png", &width, &height, &components, 0);
-	printf("width=%d, height=%d, components=%d\n", width, height, components);
+	int width, height, bytes_per_pixel;
+	unsigned char *data = stbi_load(FILENAME, &width, &height, &bytes_per_pixel, STBI_default);
+
+    if (data) {
+	    // Run through every pixel and print its greyscale value.
+		for (int row = 0; row < height; row++) {
+			int row_offset = row * width;
+
+			for (int col = 0; col < width; col++) {
+				char grey = data[row_offset + col];
+				printf("row %d, col %d: %d\n", row, col, grey);
+			}
+		}
+
+		stbi_image_free(data);
+
+		printf("width: %d\nheight: %d\nbytes per pixel: %d\n", width, height, bytes_per_pixel);
+    } else {
+    	printf("stbi_load() failed with error: \"%s\"\n", stbi_failure_reason());
+    }
+
+	return 0;
 }

a01f118473a045aa2e8ca3f62237782d08978af4
Jake Boxer <jake@github.com>
2013-11-12 00:29:00 -0800

    Get dumb image loading working

--- findbarcode.c
+++ findbarcode.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include "stb_image.c"
 
 int main(int argc, char **argv)
 {
-	printf("%s\n", "Hello, world!");
+	int width, height, components;
+	unsigned char *data = stbi_load("wikipedia_barcode.png", &width, &height, &components, 0);
+	printf("width=%d, height=%d, components=%d\n", width, height, components);
 }

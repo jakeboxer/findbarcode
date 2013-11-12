51fd163c59c9fb0d29552f2c623ca1604f89eb2d
Jake Boxer <jake@github.com>
2013-11-12 01:33:03 -0800

    Fix indentation style
    * Use tabs everywhere

--- findbarcode.c
+++ findbarcode.c
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
 	int width, height, bytes_per_pixel;
 	unsigned char *data = stbi_load(FILENAME, &width, &height, &bytes_per_pixel, STBI_default);
 
-    if (data) {
+	if (data) {
 	    // Run through every pixel and print its greyscale value.
 		for (int row = 0; row < height; row++) {
 			int row_offset = row * width;
@@ -22,9 +22,9 @@ int main(int argc, char **argv)
 		stbi_image_free(data);
 
 		printf("width: %d\nheight: %d\nbytes per pixel: %d\n", width, height, bytes_per_pixel);
-    } else {
-    	printf("stbi_load() failed with error: \"%s\"\n", stbi_failure_reason());
-    }
+	} else {
+		printf("stbi_load() failed with error: \"%s\"\n", stbi_failure_reason());
+	}
 
 	return 0;
 }
